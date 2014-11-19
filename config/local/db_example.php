bb29e92859314fa6c955047d5fad0b7acf23ff03
Stanislav Gamayunov <happyproff@gmail.com>
2014-11-20 02:16:55 +0500

    Extended db_example.php

--- config/local/db_example.php
+++ config/local/db_example.php
@@ -2,8 +2,11 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=DBNAME',
+    'username' => 'USERNAME',
+    'password' => 'PASSWORD',
     'charset' => 'utf8',
+    'tablePrefix' => '',
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => YII_ENV_DEV ? 0 : 2592000, // 1 month
 ];
